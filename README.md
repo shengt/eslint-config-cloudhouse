c976e7357f4f93f308cd90980719da9bae4575e5
Julius Cebreros <me@juliuscebreros.com>
2016-09-18 21:17:12 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CloudHouse JS Style
+
+Inherits from `airbnb`
+
+Plus
+- Use whitespace to promote readability
---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "rules": {
+    "arrow-body-style": ["warn", "always"],
+    "space-in-parens": [ "error", "always" ],
+    "array-bracket-spacing": [ "warn", "always" ],
+    "comma-dangle": [ "error", "never" ],
+    "computed-property-spacing": [ "error", "always" ],
+    "indent": ["error", 4],
+    "max-len": [ "error", 200 ],
+    "object-shorthand": [ "warn", "always" ],
+    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+    "arrow-parens": ["error", "always"]
+  }
+}
