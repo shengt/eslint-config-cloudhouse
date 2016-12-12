50817dce05f096458969c87ccda333e4f81565df
Tim Tian <tim.tian@bulletproof.net>
2016-12-12 15:48:48 +1300

    only multiple lines for trailing comma

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
   "rules": {
     "arrow-body-style": ["warn", "always"],
-    "comma-dangle": [ "error", "always" ],
-    "computed-property-spacing": [ "error", "always" ],
+    "comma-dangle": ["error", "only-multiline"],
+    "computed-property-spacing": ["error", "always"],
     "indent": ["error", 2],
-    "max-len": [ "error", 200 ],
-    "object-shorthand": [ "warn", "always" ],
+    "max-len": ["error", 200],
+    "object-shorthand": ["warn", "always"],
     "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
   }
 }
