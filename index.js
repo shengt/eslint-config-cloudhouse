3e6f261b8693440f823e60f5b3114c2a980ef60c
Tim Tian <tim.tian@bulletproof.net>
2016-12-12 15:40:48 +1300

    remove arrow spaces

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 module.exports = {
   "rules": {
     "arrow-body-style": ["warn", "always"],
-    "space-in-parens": [ "error", "always" ],
-    "array-bracket-spacing": [ "warn", "always" ],
     "comma-dangle": [ "error", "always" ],
     "computed-property-spacing": [ "error", "always" ],
     "indent": ["error", 2],
     "max-len": [ "error", 200 ],
     "object-shorthand": [ "warn", "always" ],
     "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
-    "arrow-parens": ["error", "always"]
   }
 }
