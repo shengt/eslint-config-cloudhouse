7ec5e842e066685d36a7172eee238015e041e706
Tim Tian <tim.tian@bulletproof.net>
2016-12-14 10:52:52 +1300

    remove computed property space

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
   "rules": {
     "comma-dangle": ["error", "only-multiline"],
-    "computed-property-spacing": ["error", "always"],
     "indent": ["error", 2],
     "max-len": ["error", 200],
     "object-shorthand": ["warn", "always"],
