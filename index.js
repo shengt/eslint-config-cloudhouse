a474fb3401b0f3da6452c511ffec6b67abacab15
Tim Tian <tim.tian@bulletproof.net>
2016-12-16 10:28:59 +1300

    update max len

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   "rules": {
     "comma-dangle": ["error", "only-multiline"],
     "indent": ["error", 2],
-    "max-len": ["error", 200],
+    "max-len": ["warn", 80],
     "object-shorthand": ["warn", "always"],
     "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
   }
